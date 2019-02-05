8f6c94987bc1dbef6642a26bcb8e88b865d41462
Mitali Mittal <mits254@gmail.com>
2019-02-05 10:01:03 -0600

    add readMe

--- backend/seeds/products.js
+++ backend/seeds/products.js
@@ -11,8 +11,6 @@ exports.seed = function(knex, Promise) {
         {name:'Ladder',description:" Werner 6 ft. Fiberglass Step Ladder is durable and strong. It is made from fiberglass and has a 375 lb. duty rating. ",location:"austin",type:"",price:"150",image:"https://images.homedepot-static.com/productImages/2a739d0c-7e12-45ba-85b3-6acf481c9137/svn/werner-step-ladders-fiaa06-64_1000.jpg", user_id:2},
         {name:'Wrench',description:"This tool is a pipe wrench with a chain that is used as your gripping points and is great for pipe caps or similar items. ",location:"austin",type:"",price:"40",image:"https://asrentall.com/wp-content/uploads/2013/02/pipe-wrench.png", user_id:2},
         {name:'Stud Finder',description:"Center Sensor Stud Finder has a detection depth of 1-1/2 in. for wood and metal. ",location:"austin",type:"",price:"30",image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsUzYBIn8JlTkXOwH_JCjalg-rf_7GxwkN6lsrMvskkuqtvC6O", user_id:2},
-
-        
       ]);
     });
 };
--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Footer (){
     return <footer className="site__footer">
 
-    <p>© Copyright 2017</p>
-    <p>Lorem ipsum dolor sit amet, consectetur.</p>
+    <p>©Copyright 2019</p>
+    <p>All rights reserved. Powered by Mitali Mittal</p>
   
   </footer>
 }
